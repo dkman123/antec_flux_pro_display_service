93c1f62a88be890cd92398a99ae6bcdd9a11b42e
dk <dkman123@hotmail.com>
2025-11-09 18:47:38 -0500

    Update antec_display_service.py

--- antec_display_service.py
+++ antec_display_service.py
@@ -15,7 +15,8 @@ import time
 import usb.core
 import usb.util
 
-CONFIG_FILE = "/home/dkman/git/antec_flux_pro_display_service/sensors.conf"
+CONFIG_FILE = "/etc/antec/sensors.conf"
+#CONFIG_FILE = "/home/dkman/git/antec_flux_pro_display_service/sensors.conf"
 
 def load_config():
     """
